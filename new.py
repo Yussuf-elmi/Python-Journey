27fb4331889ba74f3393fb8068ecd28a691d958a
Yussuf Elmi <yussuf.a.elmi@gmail.com>
2026-02-01 12:15:57 +0300

    Add files via upload
    intial commit

---
+++ new.py
@@ -0,0 +1,31 @@
+
+def add_numbers(a, b):
+    return a + b
+
+
+result = add_numbers(7, 13)
+print(result)
+
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+result = multiply_numbers(8, 13)
+print(result)
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+result = subtract_numbers(20, 8)
+print(result)
+
+
+def divide_numbers(x, y):
+    return x / y
+
+
+result = divide_numbers(20, 5)
+print(result)
