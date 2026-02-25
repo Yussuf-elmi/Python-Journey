600f1e2b1fffc76d11d93c436d6190c54b7c9d74
Yussuf-elmi <yussuf.a.elmi@gmail.com>
2026-02-25 12:50:46 +0300

    Budget Calculator Program

--- new.py
+++ new.py
@@ -1,31 +1,25 @@
+# income
+YOUR_NAME = input("Enter your full name:")
+Salary = float(input("Enter monthly salary:"))
+Other_income = float(input("Enter other monthly income:"))
 
-def add_numbers(a, b):
-    return a + b
+# Expenses
+Rent = float(input("Enter monthly rent:"))
+Food = float(input("Enter Monthly food exepnses:"))
+Transport = float(input("Enter monthly transport exepnses:"))
+Entertainment = float(input("Enter monthly entertainment expenses:"))
 
-
-result = add_numbers(7, 13)
-print(result)
-
-
-def multiply_numbers(a, b):
-    return a * b
-
-
-result = multiply_numbers(8, 13)
-print(result)
-
-
-def subtract_numbers(a, b):
-    return a - b
-
-
-result = subtract_numbers(20, 8)
-print(result)
-
-
-def divide_numbers(x, y):
-    return x / y
-
-
-result = divide_numbers(20, 5)
-print(result)
+# calculations
+Total_Income = Salary + Other_income
+Total_Expenses = Rent + Food + Transport + Entertainment
+Monthly_Savings = Total_Income - Total_Expenses
+Yearly_Savings = Monthly_Savings * 12
+Saving_rate = (Monthly_Savings/Total_Income)*100
+# Output
+print("Your total monthly Income:", Total_Income)
+print("Your total monthly expenses:", Total_Expenses)
+print("Your monthly savings is:", Monthly_Savings)
+print("Your yearly Savings is :", Yearly_Savings)
+print("Your saving rate is:", Saving_rate, "%")
+print("Name:", YOUR_NAME)
+print("\033[92mThank you for trusting us with your financial calculations!\033[0m")
